2cae2c5004d68a35ed54a39420770d2208061043
rokon4eg <32700236+rokon4eg@users.noreply.github.com>
2021-11-26 18:43:43 +0300

    конвертер валют

---
+++ currency.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+from decimal import Decimal
+
+
+def convert(amount, cur_from, cur_to, date, requests):
+    result =''
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    # if requests == '':
+    response = requests.get(url, params = {'date_req': date})
+    if response.status_code == 200:
+        # print(response.content)
+        soup = BeautifulSoup(response.content, 'lxml')
+        currency = soup.find('charcode' , text=cur_to)
+        # print(currency)
+        nominal = Decimal(currency.find_next_sibling('nominal').text)
+        value = Decimal(currency.find_next_sibling('value').text.replace(',','.'))
+        result = round(amount * nominal / value,4)
+    # Использовать переданный requests
+    # ...
+    # result = Decimal('3754.8057')
+    return result  # не забыть про округление до 4х знаков после запятой
+
+
